c73d1439f14a05b41e7647509c9af36682e4f545
Devinalc <97214696+Devinalc@users.noreply.github.com>
2022-03-01 18:52:21 -0800

    added more code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("what is your robot's name?"); 
+
+console.log(playerName);
+//this will fo math and log 20
+console.log(10+10);
+//what is this?
+console.log("our robot's name is " + playerName);
+
+
+function fight(){
+    window.alert("the fight has begun!"); 
+} 
+
+//fight();
+
